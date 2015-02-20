a45b785774ca56a35fdc6299f1821d88610bc27b
Peter Hurford <peter@peterhurford.com>
2015-02-20 00:25:03 -0600

    na.rm and is.na_like

---
+++ R/na.rm.R
@@ -0,0 +1,14 @@
+#' Deletes all NA and NA-like values from a vector.
+#'
+#' @param vec vector. The vector to clean.
+#' @export
+na.rm <- function(vec)
+  vec[!is.na_like(vec)]
+
+
+#' Determines if something is NA or NA-like (blank, the string "NA", or "N/A")
+#' @param x. The vector to test.
+#' @export
+is.na_like <- function(x) {
+  is.na(x) | x == "" | x == "NA" | x == "N/A"
+}
