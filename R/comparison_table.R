1ffd9a0d24c8ae2fa4bf9a56f8755c63a3b6fd42
Peter Hurford <peter@peterhurford.com>
2016-06-01 09:52:57 -0500

    Refactor comparison_table: add class, add type inference

--- R/comparison_table.R
+++ R/comparison_table.R
@@ -1,27 +1,59 @@
 #' Compares a variable across a group, both visually and with an appropriate statistical test.
 #'
 #' @param data data frame. The data to test.
-#' @param variable. If character, the name of the variable to test.  Or you can pass a specific vector instead.
-#' @param groupby character. If character, the name of the variable to group `variable` by.  Or you can pass a specific vector instead.
-#' @param type character. "Continuous" if `variable` is continuous data (like age), or "categorical" if `variable` is categorical data (like hometown).
+#' @param variable. If character, the name of the variable to test.
+#' @param groupby character. If character, the name of the variable to group `variable` by.
+#' @param type character. "Continuous" if `variable` is continuous data (like age), or "categorical" / "discrete" if `variable` is categorical data (like hometown).
+#'   This can be automatically inferred by whether the variable is numeric or not.
 #' @aliases ctable
 #' @export
-comparison_table <- function(data = NULL, variable, groupby, type) {
-  if (type != "continuous" && type != "categorical") stop('Type not recognized.')
-  x <- if (is.character(variable)) data[[variable]] else variable
-  y <- if (is.character(groupby)) data[[groupby]] else groupby
-  if (length(x) != length(y)) stop('Lengths of x and y differ.')
-  list(
-    table = switch(type,
-      continuous = data %>% group_by_(groupby) %>% select_(variable) %>%
-        summarise_each(funs(mean(., na.rm = TRUE), sd(., na.rm = TRUE))),
-      categorical = data %>% dplyr_table_(.dots = list(variable, groupby), percent = TRUE, freq = FALSE, byrow = FALSE)
-    ),
-    stat = switch(type,
-      continuous = (x ~ y) %>% lm %>% summary,
-      categorical = chisq.test(x, y)
+comparison_table <- function(data, variable, groupby, type = NULL) {
+  variable <- deparse(substitute(variable))
+  groupby <- deparse(substitute(groupby))
+  comparison_table_(data, variable, groupby, type)
+}
+
+#' @import checkr magrittr
+comparison_table_ <- checkr::ensure(
+  pre = list(data %is% dataframe,
+    variable %is% simple_string, groupby %is% simple_string,
+    type %is% NULL || type %in% c("continuous", "discrete", "categorical")),
+  function(data, variable, groupby, type = NULL) {
+    x <- data[[variable]]
+    y <- data[[groupby]]
+    if (length(x) != length(y)) stop('Lengths of x and y differ.')
+
+    if (is.null(type)) {
+      if (is.numeric(x)) { type <- "continuous" }
+      else { type <- "categorical" }
+    }
+    if (identical(type, "discrete")) { type <- "categorical" }
+
+    out <- list(
+      table = table_for(data, variable, groupby, type),
+      stat  = stat_for(x, y, type)
     )
-  )
+    class(out) <- "comparison_table"
+    out
+  })
+
+stat_for <- function(x, y, type) {
+  if (identical(type, "continuous")) { stat_for_continuous(x, y) }
+  else { stat_for_categorical(x, y) }
+}
+stat_for_continuous <- function(x, y) {  (x ~ y) %>% lm %>% summary }
+stat_for_categorical <- function(x, y) { chisq.test(x, y) }
+
+table_for <- function(data, variable, groupby, type) {
+  if (identical(type, "continuous")) { table_for_continuous(data, variable, groupby) }
+  else { table_for_categorical(data, variable, groupby) }
+}
+table_for_continuous <- function(data, variable, groupby) {
+  data %>% dplyr::group_by_(groupby) %>% dplyr::select_(variable) %>%
+    dplyr::summarise_each(dplyr::funs(mean(., na.rm = TRUE), sd(., na.rm = TRUE)))
+}
+table_for_categorical <- function(data, variable, groupby) {
+  data %>% dplyr_table_(.dots = list(variable, groupby), percent = TRUE, freq = FALSE, byrow = FALSE)
 }
 
 #' @export
