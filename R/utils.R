6406b7bbb14d9e2f0d7ea9c68c7150f4d3c268b5
peterhurford <peter@peterhurford.com>
2017-07-23 10:19:17 -0500

    Fix printing of variables

--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,6 @@ num_na <- function(x) { sum(is.na(x)) }
 num_over_zero <- function(x) { sum(x > 0) }
 
 get_varname <- function(dot) {
-  if (identical(class(dot), "lazy")) { as.character(dot$expr) }
+  if (identical(class(dot), "lazy")) { capture.output(dot$expr) }
   else { dot }
 }
