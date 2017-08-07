5a1d3efb6988535530957f2ef1feab9e66e06936
peterhurford <peter@peterhurford.com>
2017-08-06 20:23:16 -0500

    fix cut3

--- R/cut.R
+++ R/cut.R
@@ -4,14 +4,23 @@
 #' @param cuts numeric. The number of bins to split.
 #' @export
 cut3 <- function(x, cuts) {
-  s <- sort(na.rm(x))
-  len <- length(s)
-  bin_len <- ceiling(len/cuts)
-  start <- 0
+  sorted <- sort(na.rm(x))
+  len <- length(sorted)
+  if (cuts > len) {
+    stop("Too many bins.")
+  }
+  large_bin <- ceiling(len / cuts)
+  small_bin <- floor(len / cuts)
+  bin_difference <- len - large_bin - small_bin * (cuts - 1)
+  start <- 1
   out <- list()
   for (i in seq(cuts)) {
-    stop <- start + bin_len
-    out[[i]] <- s[seq(start, stop)]
+    if (i <= bin_difference + 1) {
+      stop <- start + large_bin - 1
+    } else {
+      stop <- start + small_bin - 1
+    }
+    out[[i]] <- sorted[seq(start, stop)]
     start <- stop + 1
   }
   lapply(out, na.rm)
